64d40858130ddf7a5b690d426f505a33b944ed4a
shoma1212 <113319008+shoma1212@users.noreply.github.com>
2023-11-29 12:23:17 +0900

    Delete MyFirstApp directory

--- MyFirstApp/src/main/java/com/example/demo/repository/RecordsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Records;
-
-public interface RecordsRepository extends JpaRepository<Records, Integer> {
-
-}
