bc618be6bd86027bc4d432137d29b3c207b0f4c6
S0505 <S0505@21S0505>
2023-11-30 09:52:26 +0900

    b

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Random;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		//kadaiA();
+		kadaiB();
+	}
+
+		static void kadaiB() {
+	 Random r = new Random();
+			 int x = r.nextInt(100);
+			 System.out.println(x +"が出ました");
+		}
+}
