ad9bc84383db149fec258aa520e7f600969a16bf
G0932 <G0932@LAPTOP-OB0OS839>
2023-11-30 09:46:04 +0900

    aa

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		//kadaiB();
+	}
+
+	static void kadaiA() {
+		//ここに課題を書く。メソッド名変えてね。
+		int i;
+		for (i = 0; i < 10; i++) {
+			System.out.println("現在" + i + "回目です");
+		}
+	}
+}
